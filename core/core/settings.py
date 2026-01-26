ddaa79ddd02cd5ff7192c58cb7c4d04591eb136e
Williams Rodriguez <williams.rc04@gmail.com>
2026-01-26 16:36:50 -0500

    first account new

--- core/core/settings.py
+++ core/core/settings.py
@@ -16,11 +16,11 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "unsafe-secret-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.environ.get(
-    "ALLOWED_HOSTS",
-    "*"
-).split(",")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 
+CSRF_TRUSTED_ORIGINS = os.environ.get(
+    "CSRF_TRUSTED_ORIGINS", ""
+).split(",")
 
 # Application definition
 
