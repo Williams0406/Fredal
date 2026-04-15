978d867b380335dc4ffc0d034e7927c696e87c55
Williams Rodriguez <williams.rc04@gmail.com>
2026-04-15 16:39:11 -0500

    Third commit

--- fredal-tech-app/lib/constants.js
+++ fredal-tech-app/lib/constants.js
@@ -1,8 +1,12 @@
 export const ACCESS_TOKEN_KEY = 'access_token';
 export const REFRESH_TOKEN_KEY = 'refresh_token';
 
+const LOCAL_API_URL = 'http://localhost:8000';
+const PRODUCTION_API_URL = 'https://frdl-production-47cd.up.railway.app';
+
 export const API_URL =
-  process.env.EXPO_PUBLIC_API_URL || 'http://localhost:8000';
+  process.env.EXPO_PUBLIC_API_URL ||
+  (__DEV__ ? LOCAL_API_URL : PRODUCTION_API_URL);
 
 export const ROLES = {
   TECNICO: 'Tecnico',
